adb4bd824b0fe7389fa0d9a731dd321468af9403
Sydney Lim <sydneylim@ucsb.edu>
2021-02-17 02:59:47 -0800

    without weights

--- NBC.py
+++ NBC.py
@@ -29,8 +29,8 @@ def main():
     correct = np.sum(np.equal(resultClass, validClass))
     total = len(validClass)
 
-    print(*resultClass, sep = "\n")
-    # print(correct/total)
+    # print(*resultClass, sep = "\n")
+    print(correct/total)
     
 
 
@@ -138,13 +138,13 @@ class NBC:
             return self.Dict[classVal][feature].pdf(featureVal)
         
         else:
-            # if feature in ['sex', 'patient_type', 'intubed', 'pneumonia', \
-            #         'pregnancy', 'diabetes', 'copd', 'asthma', 'inmsupr', \
-            #         'hypertension', 'other_disease', 'cardiovascular', \
-            #         'obesity', 'renal_chronic', 'tobacco', 'contact_other_covid', \
-            #         'covid_res', 'icu']:
-            #     if featureVal == 0:
-            #         return 1
+            if feature in ['sex', 'patient_type', 'intubed', 'pneumonia', \
+                    'pregnancy', 'diabetes', 'copd', 'asthma', 'inmsupr', \
+                    'hypertension', 'other_disease', 'cardiovascular', \
+                    'obesity', 'renal_chronic', 'tobacco', 'contact_other_covid', \
+                    'covid_res', 'icu']:
+                if featureVal == 0:
+                    return 1
 
             y = self.classCount(classVal)
             x = self.Dict[classVal][feature, featureVal]
