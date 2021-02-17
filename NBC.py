35c9b1b6e2e3fefa699235177419d240f8fb7ddd
Sydney Lim <sydneylim@ucsb.edu>
2021-02-17 02:36:58 -0800

    88.02

--- NBC.py
+++ NBC.py
@@ -1,29 +1,15 @@
 import numpy as np
 import pandas as pd
 import sys
+from datetime import datetime as dt
+from scipy.stats import norm, skewnorm
+import random
 
 def main():
     train = sys.argv[1]
     valid = sys.argv[2]
 
-    df = pd.read_csv(valid)
-
-    df.loc[df['date_died'] != '9999-99-99', 'date_died'] = 1
-    df.loc[df['date_died'] == '9999-99-99', 'date_died'] = 0
-
-    for col in ['sex', 'patient_type', 'intubed', 'pneumonia', \
-                'pregnancy', 'diabetes', 'copd', 'asthma', 'inmsupr', \
-                'hypertension', 'other_disease', 'cardiovascular', \
-                'obesity', 'renal_chronic', 'tobacco', 'contact_other_covid', \
-                'covid_res', 'icu']:
-
-        df.loc[np.logical_and(df[col] != 1, df[col] != 2), col] = 0
- 
-    df.loc[df['age'] <= 18, 'age'] = 1
-    df.loc[np.logical_and(df['age'] <= 30, df[col] > 18), 'age'] = 2
-    df.loc[np.logical_and(df['age'] <= 65, df[col] > 30), 'age'] = 3
-    df.loc[df['age'] > 65, 'age'] = 4
-
+    df = preprocess(valid)
 
     nbc = NBC(train, valid)
 
@@ -48,7 +34,30 @@ def main():
     
 
 
+def preprocess(filename):
 
+    df = pd.read_csv(filename)
+
+    df['entry_date'] = [dt.strptime(date, '%d-%m-%Y').toordinal() if '-' in date else dt.strptime(date, '%d/%m/%Y').toordinal() for date in df['entry_date'].tolist()]
+    df['date_symptoms'] = [dt.strptime(date, '%d-%m-%Y').toordinal() if '-' in date else dt.strptime(date, '%d/%m/%Y').toordinal() for date in df['date_symptoms'].tolist()]
+
+    df.loc[df['date_died'] != '9999-99-99', 'date_died'] = 1
+    df.loc[df['date_died'] == '9999-99-99', 'date_died'] = 0
+
+    for col in ['sex', 'patient_type', 'intubed', 'pneumonia', \
+                'pregnancy', 'diabetes', 'copd', 'asthma', 'inmsupr', \
+                'hypertension', 'other_disease', 'cardiovascular', \
+                'obesity', 'renal_chronic', 'tobacco', 'contact_other_covid', \
+                'covid_res', 'icu']:
+
+        df.loc[np.logical_and(df[col] != 1, df[col] != 2), col] = 0
+
+    # df.loc[df['age'] <= 18, 'age'] = 1
+    # df.loc[np.logical_and(df['age'] <= 30, df[col] > 18), 'age'] = 2
+    # df.loc[np.logical_and(df['age'] <= 65, df[col] > 30), 'age'] = 3
+    # df.loc[df['age'] > 65, 'age'] = 4
+
+    return df
         
 
 class NBC:
@@ -65,24 +74,7 @@ class NBC:
 
     def CSVtoDict(self):
 
-        df = pd.read_csv(self.train)
-
-
-        df.loc[df['date_died'] != '9999-99-99', 'date_died'] = 1
-        df.loc[df['date_died'] == '9999-99-99', 'date_died'] = 0
-
-        for col in ['sex', 'patient_type', 'intubed', 'pneumonia', \
-                    'pregnancy', 'diabetes', 'copd', 'asthma', 'inmsupr', \
-                    'hypertension', 'other_disease', 'cardiovascular', \
-                    'obesity', 'renal_chronic', 'tobacco', 'contact_other_covid', \
-                    'covid_res', 'icu']:
-
-            df.loc[np.logical_and(df[col] != 1, df[col] != 2), col] = 0
-
-        df.loc[df['age'] <= 18, 'age'] = 1
-        df.loc[np.logical_and(df['age'] <= 30, df[col] > 18), 'age'] = 2
-        df.loc[np.logical_and(df['age'] <= 65, df[col] > 30), 'age'] = 3
-        df.loc[df['age'] > 65, 'age'] = 4
+        df = preprocess(self.train)
 
         for col in df:
             if col not in ['entry_date', 'date_symptoms']:
@@ -92,8 +84,20 @@ class NBC:
 
                     #Dict[(col, val)] = sum(df[col] == val)
 
+        self.Dict['survived']['age'] = norm(np.mean(df.loc[df['date_died'] == 0]['age'].tolist()), np.std(df.loc[df['date_died'] == 0]['age'].tolist()))
+        a, loc, scale = skewnorm.fit(df.loc[df['date_died'] == 0]['entry_date'].tolist())
+        self.Dict['survived']['entry_date'] = skewnorm(a, loc, scale)
 
+        a, loc, scale = skewnorm.fit(df.loc[df['date_died'] == 0]['date_symptoms'].tolist())
+        self.Dict['survived']['date_symptoms'] = skewnorm(a, loc, scale)
 
+        self.Dict['died']['age'] = norm(np.mean(df.loc[df['date_died'] == 1]['age'].tolist()), np.std(df.loc[df['date_died'] == 1]['age'].tolist()))
+
+        a, loc, scale = skewnorm.fit(df.loc[df['date_died'] == 1]['entry_date'].tolist())
+        self.Dict['died']['entry_date'] = skewnorm(a, loc, scale)
+
+        a, loc, scale = skewnorm.fit(df.loc[df['date_died'] == 1]['date_symptoms'].tolist())
+        self.Dict['died']['date_symptoms'] = skewnorm(a, loc, scale)
         # for key in Dict:
         #     print(key,':', Dict['date_died'][key])
 
@@ -130,12 +134,21 @@ class NBC:
     def condP(self, feature, featureVal, classVal):
         # x is feature and its value
         # y is class
+        if feature in ['age', 'entry_date', 'date_symptoms']:
+            return self.Dict[classVal][feature].pdf(featureVal)
+        
+        else:
+            if feature in ['sex', 'patient_type', 'intubed', 'pneumonia', \
+                    'pregnancy', 'diabetes', 'copd', 'asthma', 'inmsupr', \
+                    'hypertension', 'other_disease', 'cardiovascular', \
+                    'obesity', 'renal_chronic', 'tobacco', 'contact_other_covid', \
+                    'covid_res', 'icu']:
+                if featureVal == 0:
+                    return 1
 
-        y = self.classCount(classVal)
-
-        x = self.Dict[classVal][feature, featureVal]
-
-        return x/y
+            y = self.classCount(classVal)
+            x = self.Dict[classVal][feature, featureVal]
+            return x/y
 
 
     # naive bayes classifier
@@ -143,19 +156,18 @@ class NBC:
     def classifier(self, x):
         pSurvivedGivenX = self.classP('survived')
         for feature, featureVal in x:
-            if feature not in ['entry_date', 'date_symptoms', 'date_died']:
+            if feature not in ['date_died']:
                 pSurvivedGivenX *= self.condP(feature, featureVal, 'survived')
 
 
         pDiedGivenX = self.classP('died')
         for feature, featureVal in x:
-            if feature not in ['entry_date', 'date_symptoms', 'date_died']:
+            if feature not in ['date_died']:
                 pDiedGivenX *= self.condP(feature, featureVal, 'died')
 
         pSurvivedGivenXNormalized = pSurvivedGivenX/(pSurvivedGivenX + pDiedGivenX)
         pDiedGivenXNormalized = pDiedGivenX/(pSurvivedGivenX + pDiedGivenX)
 
-        classification = 0
 
         if(pSurvivedGivenXNormalized > pDiedGivenXNormalized):
             # print("Patient will survive with a ", pSurvivedGivenXNormalized * 100, "% percent chance.")
@@ -167,7 +179,8 @@ class NBC:
 
         else:
             # print("Patient has an equal chance of living and dying.")
-            classification = 0
+            classification = random.randint(0, 1)
+
         
         return classification
 
